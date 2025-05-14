7030501f3260c17863b11217a81869d134d0a6c0
thanhvodev <nthanhatee@gmail.com>
2025-05-14 10:11:18 +0700

    feat: add emit event after storing to storage

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,13 +9,27 @@ pragma solidity 0.8.19;
  * @dev This implements the Chainlink VRF Version 2
  */
 contract Raffle {
+    /* Errors Errors*/
+    error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value <= i_entranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
